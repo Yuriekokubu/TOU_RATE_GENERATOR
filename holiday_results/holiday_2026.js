25a3f1c563d7825650a2918dc63498a5942a5d9a
Yuriekokubu <60240233118@udru.ac.th>
2026-01-05 11:59:12 +0700

    add TOU 2026

---
+++ holiday_results/holiday_2026.js
@@ -0,0 +1,235 @@
+const holiday_2026 = {
+  "data": [
+    {
+      "holiday": "01/01/2569",
+      "holidayen": "01/01/2026",
+      "holidaydtm": null,
+      "description": "วันขึ้นปีใหม่",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 0
+    },
+    {
+      "holiday": "01/05/2569",
+      "holidayen": "01/05/2026",
+      "holidaydtm": null,
+      "description": "วันแรงงานแห่งชาติ",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "01/06/2569",
+      "holidayen": "01/06/2026",
+      "holidaydtm": null,
+      "description": "ชดเชยวันวิสาขบูชา",
+      "tou": 0,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "02/01/2569",
+      "holidayen": "02/01/2026",
+      "holidaydtm": null,
+      "description": "วันหยุดราชการเพิ่มเติมเป็นกรณีพิเศษ(ตามมติคณะรัฐมนตรี เมื่อวันที่ 12 พฤศจิกายน 2567)",
+      "tou": 0,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 0
+    },
+    {
+      "holiday": "03/03/2569",
+      "holidayen": "03/03/2026",
+      "holidaydtm": null,
+      "description": "วันมาฆบูชา",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "03/06/2569",
+      "holidayen": "03/06/2026",
+      "holidaydtm": null,
+      "description": "วันเฉลิมพระชนมพรรษาสมเด็จพระนางเจ้าสุทิดา พัชรสุธาพิมลลักษณพระบรมราชินี",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "04/05/2569",
+      "holidayen": "04/05/2026",
+      "holidaydtm": null,
+      "description": "วันฉัตรมงคล",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "06/04/2569",
+      "holidayen": "06/04/2026",
+      "holidaydtm": null,
+      "description": "วันพระบาทสมเด็จพระพุทธยอดฟ้าจุฬาโลกมหาราช และ วันที่ระลึกมหาจักรีบรมราชวงศ์",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "07/12/2569",
+      "holidayen": "07/12/2026",
+      "holidaydtm": null,
+      "description": "ชดเชยวันคล้ายวันพระบรมราชสมภพของพระบาทสมเด็จพระบรมชนกาธิเบศรมหาภูมิพลอดุยเดชมหาราช บรมนาถบพิตร วันชาติ และ วันพ่อแห่งชาติ",
+      "tou": 0,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "10/12/2569",
+      "holidayen": "10/12/2026",
+      "holidaydtm": null,
+      "description": "วันรัฐธรรมนูญ",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "12/08/2569",
+      "holidayen": "12/08/2026",
+      "holidaydtm": null,
+      "description": "วันเฉลิมพระชนมพรรษาสมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ พระบรมราชชนนีพันปีหลวง และ วันแม่แห่งชาติ",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "13/04/2569",
+      "holidayen": "13/04/2026",
+      "holidaydtm": null,
+      "description": "วันสงกรานต์",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "13/05/2569",
+      "holidayen": "13/05/2026",
+      "holidaydtm": null,
+      "description": "วันพระราชพิธีจรดพระนังคัลแรกนาขวัญ",
+      "tou": 0,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "13/10/2569",
+      "holidayen": "13/10/2026",
+      "holidaydtm": null,
+      "description": "วันคล้ายวันสววคตของพระบาทสมเด็จพระบรมชนกาธิเบศรมหาภูมิพลอดุยเดชมหาราช บรมนาถบพิตร",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "14/04/2569",
+      "holidayen": "14/04/2026",
+      "holidaydtm": null,
+      "description": "วันสงกรานต์",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "15/04/2569",
+      "holidayen": "15/04/2026",
+      "holidaydtm": null,
+      "description": "วันสงกรานต์",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "23/10/2569",
+      "holidayen": "23/10/2026",
+      "holidaydtm": null,
+      "description": "วันปิยมหาราช",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "28/07/2569",
+      "holidayen": "28/07/2026",
+      "holidaydtm": null,
+      "description": "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระปรเมนทรราธิบดีศรีสินทรมหาวชิราลงกรณ์ พระวชิรเกล้าเจ้าอยู่หัว",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "29/07/2569",
+      "holidayen": "29/07/2026",
+      "holidaydtm": null,
+      "description": "วันอาสฬหบูชา",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "30/07/2569",
+      "holidayen": "30/07/2026",
+      "holidaydtm": null,
+      "description": "วันเข้าพรรษา",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    },
+    {
+      "holiday": "31/12/2569",
+      "holidayen": "31/12/2026",
+      "holidaydtm": null,
+      "description": "วันสิ้นปี",
+      "tou": 1,
+      "status": 1,
+      "updateby": null,
+      "year": 2026,
+      "edit": 1
+    }
+  ]
+};
